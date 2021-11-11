b9fb7d9779c609a81c7a574d18ddb7c7a01a4789
Lu Silva <lusilva.91@protonmail.com>
2021-11-11 17:34:22 -0300

    Refactored code: login.js

--- src/js/login.js
+++ src/js/login.js
@@ -25,47 +25,42 @@ const validPassword = document.querySelector("#userPass").addEventListener("keyd
 /* Login */
 
 // Receber os valores do form
-const formSubmitedValues = () => {
+document.getElementById('buttonIn').addEventListener('click', function (){
   let user = parseInt(document.getElementById("userAccount").value);
   let password = parseInt(document.getElementById("userPass").value);
 
   validateLogin(user, password);
-}
-document.getElementById('buttonIn').addEventListener('click', formSubmitedValues, true);
-
+}, true);
 
 // Receber os valores e confirmar se a conta existe
 const validateLogin = (user, password) => {
-  let status;
   let confirmUser;
   let confirmPassword;
 
   const checkedUsers = dataset.filter((users) => {
-    users.userAccount == user ? confirmUser = true : status = "Conta não encontrada!"
+    users.userAccount == user ? confirmUser = true : false
   })
 
   const chekedPasswords = dataset.filter((users) => {
-    users.password == password ? confirmPassword = true : status = "Senha inválida!"
+    users.password == password ? confirmPassword = true : false
   })
-  isLoginConfirmed(confirmPassword, confirmUser, status);
+  isLoginConfirmed(confirmPassword, confirmUser);
 } 
 
 //Confirmar o login
-const isLoginConfirmed = (confirmPassword, confirmUser, status) => {
+const isLoginConfirmed = (confirmPassword, confirmUser) => {
+  let status;
   let location;
 
   if (confirmPassword == true && confirmUser == true){
     status = alert("Login realizado com sucesso!");
     location = window.location = './bank.html';
   } else {
-    status = alert("Dados incorretos. Tente novamente");
+    status = alert("Dados incorretos ou insuficientes. Tente novamente");
     location = window.location = './index.html';
   }
   return `${status} ${location}`;
 }
 
 //Cadastrar new user
-const newUser = () => {
-  window.location = "./registration.html";
-};
-document.getElementById('btnNewUser').addEventListener('click', newUser, true);
+document.getElementById('btnNewUser').addEventListener('click', () => {window.location = "./registration.html"}, true);
